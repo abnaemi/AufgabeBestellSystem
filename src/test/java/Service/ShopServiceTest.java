5e2c27459425c0f95749d3e07e93ec867d48a644
abnaemi <ab.naemi@gmail.com>
2023-03-10 15:31:58 +0100

    Changed Tests

--- src/test/java/Service/ShopServiceTest.java
+++ src/test/java/Service/ShopServiceTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class ShopServiceTest {
 
     @org.junit.jupiter.api.Test
-    void getProduct() {
+    void shoudReturnTheSamegetProduct() {
 
         ShopService shop = new ShopService();
         Product product = new Product(1,"Smartphone");
@@ -29,20 +29,63 @@ class ShopServiceTest {
     }
 
     @org.junit.jupiter.api.Test
-    void addOrder() {
+    void shoudNotReturnTheSamegetProduct() {
+
+        ShopService shop = new ShopService();
+        Product product = new Product(1,"Smartphone");
+        Product product2 = new Product(2,"Notebook");
+        Product product3 = new Product(3,"Camera");
+
+
+
+        int id =1;
+        Product expected = product3 = new Product(3,"Camera");
+        Product actual = shop.getProduct(id);
+
+        assertNotEquals(expected,actual);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void returnTrueaddOrder() {
 
         ShopService shop = new ShopService();
         Product randomproduct = new Product(3,"Camera");
-        Order orderactual = new Order(1,randomproduct);
+        Product randomproduct2 = new Product(3,"Camera");
+
+        Order orderactual = new Order(1,randomproduct2);
         Order orderexpected = new Order(1,randomproduct);
 
         shop.addOrder(orderexpected);
-        shop.addOrder(orderexpected);
+        shop.addOrder(orderactual);
         shop.getOrder(0);
 
 
         System.out.println(shop.getOrder(0));
         assertEquals(orderexpected,shop.getOrder(1));
+
+        // assertEquals(expected,actual);
+
+
+
+    }
+    @org.junit.jupiter.api.Test
+    void returnNotTrueaddOrder() {
+
+        ShopService shop = new ShopService();
+        Product randomproduct = new Product(3,"Camera");
+        Product randomproduct2 = new Product(3,"Kamera");
+
+        Order orderactual = new Order(1,randomproduct2);
+        Order orderexpected = new Order(1,randomproduct);
+
+        shop.addOrder(orderexpected);
+        shop.addOrder(orderactual);
+        shop.getOrder(0);
+
+
+        System.out.println(shop.getOrder(0));
+        assertNotEquals(orderexpected,shop.getOrder(1));
 
 
         // assertEquals(expected,actual);
@@ -51,21 +94,46 @@ class ShopServiceTest {
 
     }
 
+
+
     @org.junit.jupiter.api.Test
-    void getOrder() {
+    void ReturnTruegetOrder() {
 
         ShopService shop = new ShopService();
         Product randomproduct = new Product(3,"Camera");
-        Order orderactual = new Order(1,randomproduct);
+        Product randomproduct2 = new Product(3,"Camera");
+        Order orderactual = new Order(1,randomproduct2);
         Order orderexpected = new Order(1,randomproduct);
         shop.addOrder(orderexpected);
-        shop.addOrder(orderexpected);
-        shop.addOrder(orderexpected);
+        shop.addOrder(orderactual);
+
 
 
 
         System.out.println(shop.getOrder(0));
         assertEquals(shop.getOrder(0),shop.getOrder(1));
+
+
+
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void ReturnFalsegetOrder() {
+
+        ShopService shop = new ShopService();
+        Product randomproduct = new Product(3,"Camera");
+        Product randomproduct2 = new Product(3,"Kamera");
+        Order orderactual = new Order(1,randomproduct2);
+        Order orderexpected = new Order(1,randomproduct);
+        shop.addOrder(orderexpected);
+        shop.addOrder(orderactual);
+
+
+
+
+        System.out.println(shop.getOrder(0));
+        assertNotEquals(shop.getOrder(0),shop.getOrder(1));
 
 
 
