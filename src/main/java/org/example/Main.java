7ef33f3849818818aa897d99002fb989333271fd
abnaemi <ab.naemi@gmail.com>
2023-03-10 13:03:39 +0100

    Finished Task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -24,10 +24,42 @@ public class Main {
             } else {
                 System.out.println("Wrong input, try again.");
             }
+ try {
+        System.out.println("Do you want to give an order?");
+        String orderOption = scanner.nextLine();
+        if (orderOption.equals("yes")) {
+            System.out.println("What do you want to order? Type first the ID");
+            int orderIDoptionTwo = scanner.nextInt();
+            scanner.nextLine(); // Consume the remaining newline character
+            System.out.println("Now type the product");
+            String orderOptionTwo = scanner.nextLine();
+            Product productone = new Product(orderIDoptionTwo, orderOptionTwo);
+            System.out.println("Order created: " + productone);
+            Order orderOne = new Order(1, productone);
+            shop.addOrder(orderOne);
+            System.out.println("Order created");
+        }} catch (Exception e){
+     System.out.println("Wrong input, start again");
+ }
+        System.out.println("Now you can watch your orders, press 0 for your first order and 1 for your second. If none available, it will exit");
+        try {
+            int orderNumber = scanner.nextInt();
+            scanner.nextLine(); // Consume the remaining newline character
+            shop.getOrder(orderNumber);
+        } catch (Exception e) {
+            System.out.println("Order number invalid");
         }
 
+        System.out.println("Want to see all orders?");
+        String allOrders = scanner.nextLine();
+        if (allOrders.equals("yes")) {
+            shop.listOrders();
+        }
+    }}}
 
 
 
-    }
-}
+
+
+
+
