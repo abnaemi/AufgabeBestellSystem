e2ed59830a915abb09cc166c7e3ede27f1d6ead0
abnaemi <ab.naemi@gmail.com>
2023-03-10 12:40:25 +0100

    Can choopse between all or one option. Added exception if wrong id

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,33 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
         // Created Shop
         ShopService shop = new ShopService();
 
-  // Created first Product
-   Product productOne = new Product(1, "Smartphone");
-    // Created two orders
-    Order orderOne = new Order(1,productOne);
-    Order orderTwo = new Order(1,productOne);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Hello, if you want to view all items press: all If you want to view products by ID press id and if you want to leave press exit");
+            String option = scanner.nextLine();
+            if (option.equals("all")) {
+                System.out.println(shop.listProducts());
+            } else if (option.equals("id")) {
+                try {System.out.println("Enter the ID");
+                int id = scanner.nextInt();
+                System.out.println(shop.getProduct(id));} catch (Exception e) {
+                    System.out.println("ID does not exist");
+                    continue;}
+            } else if (option.equals("exit")) {
+                break;
+            } else {
+                System.out.println("Wrong input, try again.");
+            }
+        }
 
-    // Created order list instead of two new Orders
-    ProductRepo orderList = new ProductRepo();
-    orderList.add(productOne);
-    orderList.add(productOne);
 
-    // added orderOne two the orderRepo, which is created when shop is created
-    shop.addOrder(orderOne);
-    shop.addOrder(orderTwo);
 
-        System.out.println( shop.getProduct(0));
 
     }
 }
