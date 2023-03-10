63fd5463ef57b9c0a62d8789181a31feb1623437
abnaemi <ab.naemi@gmail.com>
2023-03-10 17:50:20 +0100

    Changing User input.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,34 +13,34 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Hello, if you want to view all items press: all If you want to view products by ID press id and if you want to leave press exit");
+            System.out.println("Press all for all products. Press id to search for specific ids. Press exit to exit");
             String option = scanner.nextLine();
             if (option.equals("all")) {
                 System.out.println(shop.listProducts());
             } else if (option.equals("id")) {
-                try {System.out.println("Enter the ID");
+                try {System.out.println("Enter the ID of the Product you search");
                 int id = scanner.nextInt();
                 System.out.println(shop.getProduct(id));} catch (Exception e) {
-                    System.out.println("ID does not exist");
+                    System.out.println("This ID does not exist");
                     continue;}
             } else if (option.equals("exit")) {
                 break;
             } else {
-                System.out.println("Wrong input, try again.");
+                System.out.println("Input does not equal name or id, try again.");
             } }
 
 
 
         while (true){ try  {
-        System.out.println("Do you want to give an order?");
+        System.out.println("Do you want to order?");
         String orderOption = scanner.nextLine();
             if (orderOption.equals("no")){
                 break;}
         if (orderOption.equals("yes")) {
-            System.out.println("What do you want to order? Type first the ID");
+            System.out.println("What do you want to order? Please enter the ID.");
             int orderIDoptionTwo = scanner.nextInt();
             scanner.nextLine();
-            System.out.println("Now type the product");
+            System.out.println("Please enter the product name");
             String orderOptionTwo = scanner.nextLine();
             Product productone = new Product(orderIDoptionTwo, orderOptionTwo);
             System.out.println("Order created: " + productone);
@@ -50,7 +50,7 @@ public class Main {
                 shop.addOrder(orderOne);
                 System.out.println("Order created");
             } else {
-                System.out.println("Invalid Product name or id");
+                System.out.println("Invalid name or id");
             }
 
 
@@ -59,7 +59,7 @@ public class Main {
  }}
 
 while (true) {
-    System.out.println("Now you can view your oders. Input a number. First order equals 0. You can leave the programm in the next step");
+    System.out.println("You can view your order history. Input a number. First order equals 0. You can leave the programm in the next step");
     try {
         int orderNumber = scanner.nextInt();
         System.out.println(shop.getOrder(orderNumber));
@@ -67,7 +67,7 @@ while (true) {
         System.out.println("Order number invalid");
     }
 
-    System.out.println("Want to see all orders? (yes,no for exit)");
+    System.out.println("List all orders? (input yes or no for exit)");
     Scanner scanner2 = new Scanner(System.in);
     String allOrdersInput = scanner2.nextLine();
     if (allOrdersInput.equals("yes")) {
